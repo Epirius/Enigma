6af5f6f60e29758f0f0be642b2d39ddff05bacf7
Epirius <68005825+Epirius@users.noreply.github.com>
2021-09-19 12:55:25 +0200

    added the rotor and reflector settings.

--- Enigma(NEW).py
+++ Enigma(NEW).py
@@ -1,1 +1,23 @@
-#enigma 2.0
+#enigma M3 (army, navy)
+#https://www.cryptomuseum.com/crypto/enigma/index.htm
+#https://piotte13.github.io/enigma-cipher/
+
+Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Rotor1 = ("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q")
+Rotor2 = ("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E")
+Rotor3 = ("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V")
+Rotor4 = ("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J")
+Rotor5 = ("VZBRGITYUPSDNHLXAWMJQOFECK", "Z")
+
+ReflectorB = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
+ReflectorC = "FVPJIAOYEDRZXWGCTKUQSBNMHL"
+
+
+reflector_dict = {}
+def create_reflector(version):
+	for i in range(26):
+		reflector_dict.setdefault(Alphabet[i], version[i])
+
+
+#TODO: make this non hardcoded!!!
+create_reflector(ReflectorB)
