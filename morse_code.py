599c731c8b2976dabe8fb36e6e8d276a9ec90644
Epirius <68005825+Epirius@users.noreply.github.com>
2021-10-01 22:15:48 +0200

    created a morse code converter. it has not been implemented into the main program yet

--- morse_code.py
+++ morse_code.py
@@ -78,3 +78,48 @@ morse_dict = {
 	"-----":"0",
 
 	}
+
+def is_morse(string):
+	if len(string) == 0:
+		return False
+	for x in string:
+		if x not in '-. ':
+			return False
+	return True
+
+def convert_morse(string):
+	result = ""
+	string = string.upper()
+
+	if is_morse(string):
+		string_storage = ""
+		string += " "
+
+		for x in string:
+			if x != " ":
+				string_storage += x
+
+			elif x == " ":
+				if len(string_storage) > 0:
+					result += morse_dict[string_storage]
+				result += " "
+				string_storage = ""
+				continue
+		
+		result = result.replace("  ", "%")
+		result = result.replace(" ", "")
+		result = result.replace("%", " ")
+		return result
+
+
+	else:
+		for x in string:
+			if x == " ":
+				result += x
+				result += " "
+				continue
+			else:
+				result += morse_dict[x]
+				result += " "
+				continue
+		return result
